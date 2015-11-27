c3e6a314900c8e350de5b3dcf187d9941ed59db5
Nicolas Chambrier <naholyr@gmail.com>
2015-11-27 15:39:24 +0100

    18: Dev/Prod - Ignore redux-logger in production build

--- src/helpers/redux-logger.js
+++ src/helpers/redux-logger.js
@@ -1,3 +1,11 @@
 import createLogger from 'redux-logger'
 
-export default () => createLogger()
+export default () => {
+  if (typeof createLogger === 'function') {
+    // Module not ignored: OK
+    return createLogger()
+  } else {
+    // Return a "no-op" middleware
+    return () => (next) => (action) => next(action)
+  }
+}
