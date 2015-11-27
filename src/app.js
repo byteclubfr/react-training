235a467c4b1426754301fc18fa4438d99e229b5a
Nicolas Chambrier <naholyr@gmail.com>
2015-11-27 13:26:56 +0100

    15: outside → app (example with async data fetching)

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import { render } from 'react-dom'
 import App from './components/App'
 
 
-var contacts = [
+var root = <App initialContacts={ [] } />
+var app = render(root, document.getElementById('app'))
+
+// Asynchronously load data
+setTimeout(() => app.setState({contacts: [
   { name: "John" },
   { name: "Lilian", friend: true },
   { name: "Thomas", friend: true },
   { name: "Bruno", friend: true },
   { name: "Nicolas", friend: true }
-]
-
-var root = <App initialContacts={ contacts } />
-render(root, document.getElementById('app'))
+]}), 500)
