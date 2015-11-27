5fd2394236839322309f7002220cd76aa9c8a99a
Nicolas Chambrier <naholyr@gmail.com>
2015-11-27 09:27:52 +0100

    2: compose components

--- static/app.js
+++ static/app.js
@@ -1,7 +1,28 @@
+function Contact (props) {
+  return React.createElement('li', {},
+    React.createElement('a', {href: '#'}, 'Contact name'))
+}
+
+function ContactList () {
+  return React.createElement('ul', {className: 'contacts'},
+    React.createElement(Contact),
+    React.createElement(Contact),
+    React.createElement(Contact),
+    React.createElement(Contact))
+}
+
+function Header () {
+  return React.createElement('header', {},
+    React.createElement('h1', {}, 'Contact List'),
+    React.createElement('nav', {},
+      React.createElement('a', {href: '#'}, 'Home'),
+      React.createElement('a', {href: '#', className: 'active'}, 'Contacts')))
+}
+
 function App () {
-  return React.createElement('header', {className: ''},
-    React.createElement('span', {}, 'Hello, '),
-    React.createElement('strong', {}, 'World'))
+  return React.createElement('div', {},
+    React.createElement(Header),
+    React.createElement(ContactList))
 }
 
 var root = React.createElement(App)
