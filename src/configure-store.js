582c74e517509f8786a85dcaf3eef852016f7693
Nicolas Chambrier <naholyr@gmail.com>
2015-11-27 15:36:54 +0100

    18: Dev/Prod - Add redux logger

--- src/configure-store.js
+++ src/configure-store.js
@@ -1,12 +1,16 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux'
 import { ui, contacts } from './reducers'
 import thunkMiddleware from 'redux-thunk'
+import loggerMiddleware from './helpers/redux-logger'
+
 
 const reducer = combineReducers({ ui, contacts })
 
-// Export a function → no more singleton!
 export default () => {
-  const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
+  const createStoreWithMiddleware = applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware()
+  )(createStore)
 
   return createStoreWithMiddleware(reducer)
 }
