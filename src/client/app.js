d222851e4221b244cd76eec451befa314adf9ad7
Kent C. Dodds <kent@doddsfamily.us>
2015-11-28 11:12:30 +0100

    fix(client): Initial fetchContacts not needed anymore, handled in router

--- src/client/app.js
+++ src/client/app.js
@@ -5,16 +5,13 @@ import configureStore from '../configure-store'
 import { fetchContacts } from '../actions/contacts'
 import * as api from './api'
 import { Router } from 'react-router'
-import routes from '../routes'
+import configureRoutes from '../configure-routes'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 
 
 const store = configureStore(window.APP_STATE)
 
-if (!window.APP_STATE) {
-  // No server-side rendering: we need to fetch initial data
-  store.dispatch(fetchContacts(api))
-}
+const routes = configureRoutes(api, store)
 
 const history = createBrowserHistory()
 const root = (
